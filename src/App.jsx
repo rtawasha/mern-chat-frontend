35e989fcf732f7b591b70361f8fbca5c00f5f822
rtawasha <rtawasha@users.noreply.github.com>
2025-02-16 11:46:58 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            {/* if user is logged in, show the chat page, else, show the login page */}
             <Route path="/chat" element={ <PrivateRoute> <Chat/> </PrivateRoute>  }  />
           </Routes>
         </Router>
