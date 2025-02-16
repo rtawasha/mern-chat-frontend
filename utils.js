118afd83e057069a12e082fb25fa4692d702ae23
rtawasha <rtawasha@users.noreply.github.com>
2025-02-16 11:58:00 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,17 +1,3 @@
-//const apiURL = "https://mern-chat-backend-0koj.onrender.com";
+const apiURL = "https://mern-chat-backend-0koj.onrender.com";
 
-//export default apiURL;
-
-
-/*
-https://mern-chat-backend-0koj.onrender.com
-
---> renders the following:
-{
-  "project": "MERN Chat App using Socket.IO",
-  "message": "Welcome to MERN Chat Application",
-  "developedBy": "MasynTech",
-  "website": "www.masynctech.com"
-}
-*/
-
+export default apiURL;
